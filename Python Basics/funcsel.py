c74f4c34436a51af5730c6168ca5c6252a00641c
SoumyadipBhowmik <bhowmik.soumyadip9@gmail.com>
2022-10-29 16:32:59 +0530

    Func

--- Python Basics/funcsel.py
+++ Python Basics/funcsel.py
@@ -1,5 +1,6 @@
 #used constructors in class
 #They are called the init methodor the magic method1 in class and object in python
+#going to go over this again
 
 class Item:
     def __init__(self, name, price = 0, quantity = 0):
