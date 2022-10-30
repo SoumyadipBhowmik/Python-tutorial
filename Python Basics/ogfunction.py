89ca1f2d80aed4cbb08bd248050cb777df63e38c
Soumyadip Bhowmik <105037882+SoumyadipBhowmik@users.noreply.github.com>
2022-10-30 12:51:16 +0530

    Update ogfunction.py

--- Python Basics/ogfunction.py
+++ Python Basics/ogfunction.py
@@ -1,3 +1,6 @@
+#Little changes have been made to the functions as I have added another function which checks if thenumbe is a palindrome or not.
+#Strange thig is this exact copy of logic doesn't apply on leetcode
+
 def reverseNum(num):
     ans = 0
     while num:
@@ -6,13 +9,16 @@ def reverseNum(num):
         num //=10
     return ans
 
+def isPalindrome(num: int) -> bool:
+    return num == reverseNum(num)
+
 num = int(input("Enter a number: "))
 
 ans = reverseNum(num)
 print(ans)
 
 #check palindrome
-if reverseNum(num) == num:
-    print("Palindrome!")
+if isPalindrome(num) == True:
+    print("Palindrome")
 else:
-    print("Not today buddy.")
+    print("What?")
